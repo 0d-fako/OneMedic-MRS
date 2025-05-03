fb21424995489477bf921121142ced9f498c4d47
0d-fako <od.fakorede@gmail.com>
2025-05-03 23:39:36 +0100

    Updated Medis

--- onemedic/src/main/java/com/onemedic/onemedic/controller/PersonController.java
+++ onemedic/src/main/java/com/onemedic/onemedic/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.onemedic.onemedic.controller;
 public class PersonController {
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService){
         this.personService = personService;
     }
